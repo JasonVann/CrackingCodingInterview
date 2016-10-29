4619ca79d27f15930046cef932ac32a79b1b4ad0
JasonVann <jasonvanet@gmail.com>
2016-10-29 00:08:10 -0700

    Added 1.2

--- C1_ArraysAndStrings/Ex1.2.py
+++ C1_ArraysAndStrings/Ex1.2.py
@@ -1,2 +1,23 @@
-def check_permu():
-    
+def check_permu(a, b):
+    if len(a) != len(b):
+        return False
+    dic = {}
+    for i in b:
+        if i in dic:
+            dic[i] += 1
+        else:
+            dic[i] = 1
+    for j in a:
+        if j not in dic:
+            return False
+        else:
+            dic[j] -= 1
+            if dic[j] == 0:
+                dic.pop(j)
+    return True
+
+a = 'abcdef'
+b = 'bcfeda'
+a = 'abaa'
+b = 'abac'
+print check_permu(a, b)
