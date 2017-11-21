31c3be35d64c53bfd3e98a5205040c6269f012c6
JasonVann <jasonvanet@gmail.com>
2017-11-21 22:52:45 +0800

    Added my implementation for Trie

---
+++ LeetCode/Ex200/Ex208.py
@@ -0,0 +1,81 @@
+# Maybe better to use Trie and TrieNode, not a nested Trie class
+# http://www.geeksforgeeks.org/trie-insert-and-search/
+
+class TrieNode:
+    def __init__(self):
+        self.child = [None]*26
+        self.is_end_of_word = False
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        #self.root = TrieNode()
+        self.child = [None]*26
+        self.is_end_of_word = False
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        if len(word) == 0:
+            self.is_end_of_word = True
+            return
+
+        idx = ord(word[0])-ord('a')
+        if self.child[idx] == None:
+            node = Trie()
+            self.child[idx] = node
+        else:
+            node = self.child[idx]
+
+        node.insert(word[1:])
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        if len(word) == 0:
+            return False
+
+        idx = ord(word[0])-ord('a')
+        if self.child[idx] == None:
+            return False
+        if len(word) == 1 and self.child[idx] and self.child[idx].is_end_of_word:
+            return True
+        return self.child[idx].search(word[1:])
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        if len(prefix) == 0:
+            return True
+
+        idx = ord(prefix[0])-ord('a')
+        if self.child[idx] == None:
+            return False
+        if len(prefix) == 1 and self.child[idx]:
+            return True
+        return self.child[idx].startsWith(prefix[1:])
+
+
+
+# Your Trie object will be instantiated and called as such:
+word = 'test'
+obj = Trie()
+obj.insert(word)
+param_2 = obj.search(word)
+print(param_2)
+print(obj.search(word[:3]))
+print(obj.search('testb'))
+#param_3 = obj.startsWith(prefix)
+print(obj.startsWith('tes'))
