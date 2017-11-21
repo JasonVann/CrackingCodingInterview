4049b02e193ae7863fc66a6f1dcf5a6470fb4b68
JasonVann <jasonvanet@gmail.com>
2017-11-22 00:02:35 +0800

    Added iterative version of Trie

--- LeetCode/Ex200/Ex208.py
+++ LeetCode/Ex200/Ex208.py
@@ -6,7 +6,7 @@ class TrieNode:
         self.child = [None]*26
         self.is_end_of_word = False
 
-class Trie:
+class Trie_Recur:
 
     def __init__(self):
         """
@@ -67,6 +67,56 @@ class Trie:
             return True
         return self.child[idx].startsWith(prefix[1:])
 
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        node = self.root
+        for i in range(len(word)):
+            idx = ord(word[i])-ord('a')
+            if node.child[idx] == None:
+                node2 = TrieNode()
+                node.child[idx] = node2
+            node = node.child[idx]
+        node.is_end_of_word = True
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        node = self.root
+        for i in range(len(word)):
+            idx = ord(word[i])-ord('a')
+            if node.child[idx] == None:
+                return False
+            node = node.child[idx]
+        return node.is_end_of_word
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        node = self.root
+        for i in range(len(prefix)):
+            idx = ord(prefix[i])-ord('a')
+            if node.child[idx] == None:
+                return False
+            node = node.child[idx]
+        return True
 
 
 # Your Trie object will be instantiated and called as such:
