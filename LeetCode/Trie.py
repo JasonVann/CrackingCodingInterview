a36d6cb4b9828b620746ccc2e7b5162ba9e4cf46
JasonVann <jasonvanet@gmail.com>
2017-11-22 12:34:23 +0800

    Added Trie

---
+++ LeetCode/Trie.py
@@ -0,0 +1,131 @@
+class TrieNode:
+    def __init__(self):
+        self.child = [None]*26
+        self.is_end_of_word = False
+
+
+class Trie_Recur:
+    # Maybe better to use Trie and TrieNode, not a nested Trie class
+    # http://www.geeksforgeeks.org/trie-insert-and-search/
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        #self.root = TrieNode()
+        self.child = [None]*26
+        self.is_end_of_word = False
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        if len(word) == 0:
+            self.is_end_of_word = True
+            return
+
+        idx = ord(word[0])-ord('a')
+        if self.child[idx] == None:
+            node = Trie()
+            self.child[idx] = node
+        else:
+            node = self.child[idx]
+
+        node.insert(word[1:])
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        if len(word) == 0:
+            return False
+
+        idx = ord(word[0])-ord('a')
+        if self.child[idx] == None:
+            return False
+        if len(word) == 1 and self.child[idx] and self.child[idx].is_end_of_word:
+            return True
+        return self.child[idx].search(word[1:])
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        if len(prefix) == 0:
+            return True
+
+        idx = ord(prefix[0])-ord('a')
+        if self.child[idx] == None:
+            return False
+        if len(prefix) == 1 and self.child[idx]:
+            return True
+        return self.child[idx].startsWith(prefix[1:])
+
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        node = self.root
+        for i in range(len(word)):
+            idx = ord(word[i])-ord('a')
+            if node.child[idx] == None:
+                node2 = TrieNode()
+                node.child[idx] = node2
+            node = node.child[idx]
+        node.is_end_of_word = True
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        node = self.root
+        for i in range(len(word)):
+            idx = ord(word[i])-ord('a')
+            if node.child[idx] == None:
+                return False
+            node = node.child[idx]
+        return node.is_end_of_word
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        node = self.root
+        for i in range(len(prefix)):
+            idx = ord(prefix[i])-ord('a')
+            if node.child[idx] == None:
+                return False
+            node = node.child[idx]
+        return True
+
+
+def test_Trie():
+    word = 'test'
+    obj = Trie()
+    obj.insert(word)
+    param_2 = obj.search(word)
+    print(param_2)
+    print(obj.search(word[:3]))
+    print(obj.search('testb'))
+    #param_3 = obj.startsWith(prefix)
+    print(obj.startsWith('tes'))
