89348bea9ad32f92b6788c44036ec40a36f3e723
JasonVann <jasonvanet@gmail.com>
2017-11-24 18:38:11 +0800

    Added peer's BFS & DP sol

--- LeetCode/Ex100/Ex139.py
+++ LeetCode/Ex100/Ex139.py
@@ -1,3 +1,38 @@
+class Peer:
+    '''
+    public class Solution {
+    public boolean wordBreak(String s, Set<String> dict) {
+        boolean[] f = new boolean[s.length() + 1];
+        f[0] = true;
+        //Second DP
+        for(int i=1; i <= s.length(); i++){
+            for(int j=0; j < i; j++){
+                if(f[j] && dict.contains(s.substring(j, i))){
+                    f[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return f[s.length()];
+    }
+}
+    '''
+    def word_break_SN(s, words):
+     	d = [False] * len(s)
+     	for i in range(len(s)):
+     		for w in words:
+     			if w == s[i-len(w)+1:i+1] and (d[i-len(w)] or i-len(w) == -1):
+     				d[i] = True
+     	return d[-1]
+
+    def wordBreak(self, s, words):
+        # O(n2)
+        ok = [True]
+        for i in range(1, len(s)+1):
+            ok += any(ok[j] and s[j:i] in words for j in range(i)),
+        return ok[-1]
+
 class Solution:
     def wordBreak(self, s, wordDict):
         """
