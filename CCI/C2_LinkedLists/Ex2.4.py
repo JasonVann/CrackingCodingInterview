f82492143f078c2abd863a35aea7a231ee5ddb34
JasonVann <jasonvanet@gmail.com>
2017-11-27 16:38:40 +0800

    Added partition

---
+++ CCI/C2_LinkedLists/Ex2.4.py
@@ -0,0 +1,29 @@
+class Node(object):
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def partition(head, k):
+    # all nodes smaller than k, k, larger than k
+    small = None
+    large = None
+    node = head
+    while node:
+        if node.val < k:
+            if small:
+                small.next = node
+            else:
+                small = node
+            small = small.next
+        else:
+            if large:
+                large.next = node
+            else:
+                large = node
+                partition = node
+            large = large.next
+        node = node.next
+    if not small:
+        return partition
+    small.next = partition
+    return head
