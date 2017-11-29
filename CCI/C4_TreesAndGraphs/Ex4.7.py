81b7c3b5be76553f6e5cc6118c46347eefb404e9
JasonVann <jasonvanet@gmail.com>
2017-11-29 11:46:13 +0800

    Added check for cyclic dependence

--- CCI/C4_TreesAndGraphs/Ex4.7.py
+++ CCI/C4_TreesAndGraphs/Ex4.7.py
@@ -12,38 +12,49 @@ def build_order(projects, dependencies):
         else:
             table_yx[y] = [x]
         if x in table_xy:
+
             table_xy[x] += [y]
         else:
             table_xy[x] = [y]
+        # Check for cyclic dependency
+        if y in table_xy and x in table_xy[y]:
+            return None
 
     for p in projects:
         if p not in table_xy and p not in table_yx:
             order.append(p)
 
     l = len(dependencies)
+    stack = []
     while True:
-        keys = list(table_xy.keys())
-        if len(keys) == 0:
-            break
-        x = keys[0]
-        
+        if stack == []:
+            keys = list(table_xy.keys())
+            if len(keys) == 0:
+                break
+            x = keys[0]
+        else:
+            x = stack.pop(0)
         while x in table_yx:
-            x = table_yx[x][0]
+            if len(table_yx[x]) == 0:
+                table_yx.pop(x)
+            else:
+                x = table_yx[x][0]
         if x not in table_yx:
-            if x not in order:
-                order.append(x)
-            for y in table_xy[x]:
-                table_yx[y].remove(x)
-                if len(table_yx[y]) == 0:
-                    table_yx.pop(y)
-                    if y not in order:
-                        order.append(y)
-            table_xy.pop(x)
+            #if x not in order:
+            order.append(x)
+            if x in table_xy:
+                for y in table_xy[x]:
+                    table_yx[y].remove(x)
+                    if len(table_yx[y]) == 0:
+                        stack.append(y)
+
+                table_xy.pop(x)
 
     return order
 
 def test():
     projects = ['a', 'b', 'c', 'd', 'e', 'f']
+    #dependencies = [('d', 'a'),('a', 'd'), ('f', 'b'), ('b', 'd'), ('f', 'a'), ('d', 'c')]
     dependencies = [('a', 'd'), ('f', 'b'), ('b', 'd'), ('f', 'a'), ('d', 'c')]
     order = build_order(projects, dependencies)
     print(order)
