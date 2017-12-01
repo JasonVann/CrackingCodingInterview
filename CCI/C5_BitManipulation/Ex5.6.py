ea27e831f86cf1b6db34f9da11831d49e17eba87
JasonVann <jasonvanet@gmail.com>
2017-12-01 11:29:25 +0800

    Added CCI sol

--- CCI/C5_BitManipulation/Ex5.6.py
+++ CCI/C5_BitManipulation/Ex5.6.py
@@ -8,7 +8,17 @@ def count_bits(a, b):
         res >>= 1
     return i
 
+def count_bits_sol(a, b):
+    res = a ^ b
+    # then count 1s in res
+    i = 0
+    while res > 0:
+        i += 1
+        res = res & (res-1)
+    return i
+
 def test():
     print(count_bits(29, 15))
+    print(count_bits_sol(29, 15))
 
 test()
