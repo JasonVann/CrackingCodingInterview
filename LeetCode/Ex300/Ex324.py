024dd60088d99c79aeb67e7566a2e12e1917ee74
JasonVann <jasonvanet@gmail.com>
2017-12-27 20:54:20 +0800

    Added tricky LC sol

--- LeetCode/Ex300/Ex324.py
+++ LeetCode/Ex300/Ex324.py
@@ -54,3 +54,32 @@ class Solution:
                 A[k] = A_right[j]
                 j += 1
             k += 1
+            
+'''
+https://leetcode.com/problems/wiggle-sort-ii/discuss/77682
+public void wiggleSort(int[] nums) {
+        int median = findKthLargest(nums, (nums.length + 1) / 2);
+        int n = nums.length;
+
+        int left = 0, i = 0, right = n - 1;
+
+        while (i <= right) {
+
+            if (nums[newIndex(i,n)] > median) {
+                swap(nums, newIndex(left++,n), newIndex(i++,n));
+            }
+            else if (nums[newIndex(i,n)] < median) {
+                swap(nums, newIndex(right--,n), newIndex(i,n));
+            }
+            else {
+                i++;
+            }
+        }
+
+
+    }
+
+    private int newIndex(int index, int n) {
+        return (1 + 2*index) % (n | 1);
+    }
+'''
